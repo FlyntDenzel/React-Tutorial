337b38c6f1653975ebaad43afbe123a626cbdb24
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-23 23:51:04 +0100

    added a style file and linked it to the index file

---
+++ firstapp/src/index.js
@@ -0,0 +1,5 @@
+//enabling the use of react and its dom
+var React = require("react");
+var ReactDom = require("react-dom");
+
+// variable.render(what to render, where to render, when render function is completed(optional))
