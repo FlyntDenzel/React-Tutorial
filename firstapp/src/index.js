9e29b33f813315e2fb1efb261513a220f167133e
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-23 23:54:24 +0100

    created first rendered elements

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -3,3 +3,5 @@ var React = require("react");
 var ReactDom = require("react-dom");
 
 // variable.render(what to render, where to render, when render function is completed(optional))
+
+ReactDom.render(<h1>Flynt Denzel</h1>, document.getElementById("root"));
