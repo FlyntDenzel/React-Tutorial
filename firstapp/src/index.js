bcebbbe7dcdc6b92d0be15a11ee7773c8da7f38b
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-24 01:08:30 +0100

    generated a random number updating in real life

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react";
+import ReactDOM from "react-dom";
+
+// const number = 4;
 
 ReactDOM.render(
   <div>
-    <h1>My favourite video games</h1>
-  <ul>
-    <li>Call of Duty</li>
-    <li>Splinter Cell</li>
-    <li>Assassin's creed</li>
-  </ul>
+      <h1>My favourite video games</h1>
+      <ul>
+       <li>Call of Duty</li>
+       <li>Splinter Cell</li>
+       <li>Assassin's creed</li>
+     </ul>
+     <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
